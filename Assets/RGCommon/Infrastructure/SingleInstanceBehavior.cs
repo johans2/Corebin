39be1f01a8feac4e6e1221692765cababad32890
JohanSvensson <js@resolutiongames.com>
2016-11-16 15:35:31 +0100

    Added project.

---
+++ Assets/RGCommon/Infrastructure/SingleInstanceBehavior.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace RGCommon {
+    /// <summary>
+    /// Use as superclass to make a single instance of a MonoBehavior available.
+    /// Unlike the standard Singleton pattern, trying to get the instance if
+    /// no instance has been created does not create it automatically.
+    /// Instead that is an error.
+    /// 
+    /// Typical usage is to create a script similar to this:
+    /// 
+    /// public class MyScript : SingleInstanceBehavior<MyScript> {
+    ///     // ...
+    /// }
+    /// 
+    /// Then add an instance of MyScript to the scene.
+    /// It will then be accessible as MyScript.Instance after the Awake phase.
+    /// 
+    /// If your script implements Awake or OnDestroy, it has to call
+    /// the corresponding method in this class.
+    /// </summary>
+    /// <typeparam name="T">The type of the instance to make available as Instance</typeparam>
+    public class SingleInstanceBehavior<T> : MonoBehaviour where T : SingleInstanceBehavior<T> {
+        private static T instance;
+
+        public virtual void Awake() {
+            Assert.IsNull(instance, "Duplicate instance of " + typeof(T) + ": " + instance + " and " + this);
+            instance = (T)this;
+        }
+
+        public virtual void OnDestroy() {
+            instance = null;
+        }
+
+        /// <summary>
+        /// Get the instance. It is an error if the instance hasn't been created.
+        /// </summary>
+        public static T Instance {
+            get {
+                Assert.IsNotNull(instance, "No instance of " + typeof(T));
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Get the instance if it exists.
+        /// </summary>
+        /// <returns>The instance, or null if it hasn't been created</returns>
+        public static T GetInstance() {
+            return instance;
+        }
+    }
+}
