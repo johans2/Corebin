5a8bd7b674895d998366b6fdebfa1756a89dd889
JohanSvensson <js@resolutiongames.com>
2016-11-18 10:23:56 +0100

    Snuuuring planets.

---
+++ Assets/Script/sneeer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sneeer : MonoBehaviour {
+
+    public float rotationSpeed = 10f;
+
+	
+	void Update () {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);    
+	}
+}
